fec75e62fd87084c81ef217ac0fa6f85058ac3d2
Samuel <samuelpierrej@gmail.com>
2026-01-29 17:31:46 -0300

    deleting unusued files

--- backend/crazy-look/src/user/exceptions.py
+++
@@ -1,6 +0,0 @@
-from src.base_exception import ErrorBase
-from fastapi import status
-
-class UserAlreadyExist(ErrorBase):
-    detail = "This user already exist"
-    status_code = status.HTTP_400_BAD_REQUEST
