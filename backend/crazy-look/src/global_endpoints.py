a18b5b544493c97728db011eef7cbad52a297c8d
Samuel <samuelpierrej@gmail.com>
2026-01-29 17:33:43 -0300

    deleting unusued files

--- backend/crazy-look/src/global_endpoints.py
+++
@@ -1,8 +0,0 @@
-from src.user.router import user_router
-from fastapi import APIRouter
-
-api_router = APIRouter()
-
-
-
-api_router.include_router(user_router)
--- backend/crazy-look/src/user/service.py
+++
@@ -1,7 +0,0 @@
-from pwdlib import PasswordHash
-
-
-password_hash = PasswordHash.recommended()
-
-def hash_password(password) -> str:
-    return password_hash.hash(password)
